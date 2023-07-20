00e7f372c6e1d0a9262587e16d99c4668d4e75b2
xplusc <keenannewcomer@gmail.com>
2023-07-20 13:47:31 -0700

    Actually finished refactoring

--- api/index.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express')
-
-const router = Router()
-
-router.use('/businesses', require('./businesses'))
-router.use('/photos', require('./photos'))
-router.use('/media', require('./media'))
-
-module.exports = router
